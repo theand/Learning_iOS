cfa9175cc1ec6465214a85395af3485826bd21ba
Heesang Chae <theand@gmail.com>
2013-01-17 12:46:16 +0900

    고급책 p8 urlscheme 테스트

---
+++ 2012-10-25/ios20121025_urlscheme/ios20121025_urlscheme/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ios20121025_urlscheme
+//
+//  Created by sdt5 on 10/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+- (IBAction)openMyUrl:(id)sender;
+
+@end
---
+++ 2012-10-25/ios20121025_urlscheme/ios20121025_urlscheme/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  ios20121025_urlscheme
+//
+//  Created by sdt5 on 10/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (IBAction)openMyUrl:(id)sender {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"myurl://kr.co.tacademy"] ] ;
+}
+
+@end
